53d86b173f30389bacc8f48cfc04133e56ea0d43
alex-bars25 <alex.bars25@gmail.com>
2022-05-02 02:24:39 +0300

    complete 9 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!options.repeatTimes)
+    options.repeatTimes = 1;
+  
+  if (!options.additionRepeatTimes)
+    options.additionRepeatTimes = 1;
+
+  if (!options.additionSeparator)
+    options.additionSeparator = '|';
+
+  let addition = [];
+  if (options.addition === undefined)
+    options.addition = '';
+  else {
+    for (i = 0; i < options.additionRepeatTimes; i++) {
+      addition.push('' + options.addition);
+    }
+    addition = addition.join(options.additionSeparator);
+  }
+
+  if (!options.separator)
+    options.separator = '+';
+
+  let string = [];
+  for (i = 0; i < options.repeatTimes; i++) {
+    string.push(str + addition);
+  }
+  return string.join(options.separator);
 }
+
+console.log(repeater('STRING', { separator: '**', addition: 'PLUS', additionSeparator: '00' }));
 
 module.exports = {
   repeater
