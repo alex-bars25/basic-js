28660a08e6e9c075ec7be4af2b36c996ece74313
alex-bars25 <alex.bars25@gmail.com>
2022-05-02 10:37:55 +0300

    complete 10 tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined)
+      throw Error('Incorrect arguments!')
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encrypted = '';
+    for (let i = 0, j = 0; i < message.length; i++){
+      if (j >= key.length)
+        j = 0;
+      if (message.charCodeAt(i) > 64 && message.charCodeAt(i) < 91){
+        let char = (message.charCodeAt(i) - 65 + key.charCodeAt(j) - 65) % 26;
+        encrypted += String.fromCharCode(char + 65);
+        j++;
+      } else {
+        encrypted += message[i];
+      }
+    }
+    if (this.type === false)
+      return encrypted.split('').reverse().join('');
+    else
+      return encrypted;
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined)
+    throw Error('Incorrect arguments!')
+
+    key = key.toUpperCase();
+    let decrypted = '';
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++){
+      if (j >= key.length)
+        j = 0;
+      if (encryptedMessage.charCodeAt(i) > 64 && encryptedMessage.charCodeAt(i) < 91){
+        let char = (26 - (key.charCodeAt(j) - 65) + (encryptedMessage.charCodeAt(i) - 65)) % 26;
+        decrypted += String.fromCharCode(char + 65);
+        j++;
+      } else {
+        decrypted += encryptedMessage[i];
+      }
+    }
+    if (this.type === false)
+      return decrypted.split('').reverse().join('');
+    else
+      return decrypted;
   }
 }
 
